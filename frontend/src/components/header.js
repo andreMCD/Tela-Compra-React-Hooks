20e277562a3c8e3973cd507b32cf0935a745dcb5
andre macedo <andredemcd@hotmail.com>
2019-08-11 01:03:03 -0300

    finalizando o marckup e style do header

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -2,29 +2,38 @@ import React from "react";
 
 import logo from "../assets/logo-header.png";
 import shoppingCart from "../assets/shopping-cart.svg";
+import lupaSearch from "../assets/search.svg";
 
-import { Head } from "../components/styles";
+import { Head } from "../style/head";
 
 export default function Header() {
   return (
     <Head>
       <img src={logo} alt="Agência N1" className="logo" />
 
-      <nav className="menu">
-        <ul>
-          <li>
-            <a href="#">Games</a>
-          </li>
-          <li>
-            <a href="#">Presentes</a>
-          </li>
-          <li>
-            <a href="#" className="link">
-              Sales
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div id="menuArea">
+        <input type="checkbox" id="menuToggle" />
+        <label htmlFor="menuToggle" className="menuOpen">
+          <div className="open" />
+        </label>
+        <div className="menu menuEffects">
+          <label htmlFor="menuToggle" />
+          <nav className="navegation">
+            <ul className="menu">
+              <li>
+                <a href="index.html">Game</a>
+              </li>
+              <li>
+                <a href="about/index.html">Presente</a>
+              </li>
+              <li>
+                <a href="contact/index.html">Sales</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+      <img src={lupaSearch} alt="Lupa pesquisar" className="lupaSearch" />
       <input type="text" className="search" placeholder="search" />
       <img src={shoppingCart} alt="Carrinho de Compras" className="logoCart" />
     </Head>
