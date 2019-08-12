4eea6fb729a13e35d8152da4d55cd088a5d793fa
andre macedo <andredemcd@hotmail.com>
2019-08-12 02:36:20 -0300

    criei objeto contentvistos e se styled components

---
+++ frontend/src/objects/contentVistos.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import { Vistos } from "./styles";
+
+export default function BoxVistos() {
+  return (
+    <Vistos>
+      <h1>Também viram</h1>
+      <ul>
+        <li>
+          <img />
+          <h2>Nome do action figure</h2>
+          <p>de R$ xxx.xx</p>
+          <p>
+            por R$ <strong>xxx.xx</strong>
+          </p>
+        </li>
+        <li>
+          <img />
+          <h2>Nome do action figure</h2>
+          <p>de R$ xxx.xx</p>
+          <p>
+            por R$ <strong>xxx.xx</strong>
+          </p>
+        </li>
+        <li>
+          <img />
+          <h2>Nome do action figure</h2>
+          <p>de R$ xxx.xx</p>
+          <p>
+            por R$ <strong>xxx.xx</strong>
+          </p>
+        </li>
+      </ul>
+    </Vistos>
+  );
+}
